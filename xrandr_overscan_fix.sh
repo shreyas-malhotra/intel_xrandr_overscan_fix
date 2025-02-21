07c8f540f13f616f3ed8e2207820924b4b038a38
Shreyas Malhotra <shreyasmalhotra2003@gmail.com>
2025-02-21 10:25:04 +0530

    Updated script to be more user-friendly for usage in the terminal, there is no need to edit the command manually now, since the display number and resolution can just be passed as command-line options now.

--- xrandr_overscan_fix.sh
+++ xrandr_overscan_fix.sh
@@ -1,1 +1,12 @@
-xrandr --output HDMI-1 --set audio force-dvi --mode 1920x1080
+#!/bin/bash
+
+if [ $# -lt 2 ]; then
+    echo "Usage: $0 <output> <mode>"
+    echo "Example: $0 HDMI-1 1920x1080"
+    exit 1
+fi
+
+OUTPUT=$1
+MODE=$2
+
+xrandr --output "$OUTPUT" --set audio force-dvi --mode "$MODE"
